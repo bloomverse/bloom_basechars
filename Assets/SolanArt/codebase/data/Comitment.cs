9020b90844b097da0057dff1d5040ed138b28cb5
bloomverse <billing@bloomverse.io>
2022-08-06 10:31:10 -0500

    Solanrt Plugin Integration

---
+++ Assets/SolanArt/codebase/data/Comitment.cs
@@ -0,0 +1,14 @@
+namespace AllArt.Solana
+{
+    [System.Serializable]
+    public class Commitment
+    {
+        public string commitment;
+    }
+
+    [System.Serializable]
+    public enum ECommitment
+    {
+        Finalized
+    }
+}
---
+++ Assets/SolanArt/packages/solnet/Solnet.Rpc/Messages/JsonRpcBase.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Solnet.Rpc.Messages
+{
+    [Serializable]
+    public class JsonRpcBase
+    {
+        public string jsonrpc;
+
+        public int id;
+    }
+}
